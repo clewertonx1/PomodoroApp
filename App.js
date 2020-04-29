406cd6c877d8c551ef752ae153913d66f4e1023c
Clewerton Roque <clewerton203@gmail.com>
2020-04-29 18:09:23 -0300

    Button start/pause

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {useEffect, useState, } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 
 
@@ -8,30 +8,44 @@ export default function App() {
   
   const [minutes, setMinutes] = useState(25)
   const [secods, setSeconds] = useState(0)
-  
+  const [status, setStatus] = useState(false)
 
-  useEffect(() => {
-    let count = 0
-    const loop = setInterval(() =>{
-      
-      if(count === 0){
-        setMinutes(minutes => minutes -1 )
-        setSeconds(60)
-        count = 60 
-      }
-      if(count > 0 ){    
-        setSeconds(secods => secods -1 )
-        count = count - 1
+
+  
+  useEffect(() =>{
+    console.log("teste")
+    console.log(status)
+    if(status){
+      let count = secods
+      const loop = setInterval(() =>{
+        console.log("teste") 
         console.log(count)
-      }
+        if(count === 0){
+          console.log("bbbb")
+          setMinutes(minutes => minutes -1 )
+          setSeconds(60)
+          count = 60 
+        }
+        if(count > 0 ){    
+          setSeconds(secods => secods -1 )
+          count = count - 1
+        }
     }, 1000)
     return () => clearInterval(loop)
-  },[])
+    }
+  },[status])
 
+
+  function handleButton(){
+    setStatus(!status)
+  }
   return (
     
     <View style={styles.container}>
       <Text>{`${minutes}:${secods}`}</Text>
+      <TouchableOpacity onPress={() => handleButton()}>
+        <Text>{status ? "Pause" : "Start"}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
