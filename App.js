a5291b465eba97fda90e35405e74fd65a364a4a1
Clewerton Roque <clewerton203@gmail.com>
2020-04-29 19:08:52 -0300

    Finished

--- App.js
+++ App.js
@@ -9,26 +9,43 @@ export default function App() {
   const [minutes, setMinutes] = useState(25)
   const [secods, setSeconds] = useState(0)
   const [status, setStatus] = useState(false)
-
+  const [intervalStatus, setIntervalStatus] = useState(false)
 
   
   useEffect(() =>{
     console.log("teste")
     console.log(status)
+
+   
+
     if(status){
-      let count = secods
+
+      if(intervalStatus){
+        setMinutes(5)
+      }else{
+        setMinutes(25)      
+      }
+
+      let countSeconds = 0
+      let countMinutes = minutes
+      console.log(countSeconds)
       const loop = setInterval(() =>{
-        console.log("teste") 
-        console.log(count)
-        if(count === 0){
-          console.log("bbbb")
+        if(countMinutes < 0 ){
+          setIntervalStatus(!intervalStatus)
+          setMinutes(5)
+          countSeconds = 0
+          setSeconds(0)
+          clearInterval(loop)    
+        }
+        if(countSeconds === 0){
           setMinutes(minutes => minutes -1 )
           setSeconds(60)
-          count = 60 
+          countMinutes = countMinutes - 1
+          countSeconds = 60
         }
-        if(count > 0 ){    
+        if(countSeconds > 0 ){    
           setSeconds(secods => secods -1 )
-          count = count - 1
+          countSeconds = countSeconds - 1
         }
     }, 1000)
     return () => clearInterval(loop)
@@ -36,14 +53,14 @@ export default function App() {
   },[status])
 
 
-  function handleButton(){
+  function changeStatus(){
     setStatus(!status)
   }
   return (
     
     <View style={styles.container}>
       <Text style={styles.text}>{`${minutes}:${secods}`}</Text>
-      <TouchableOpacity  style={[styles.button, {backgroundColor: status ? '#ff7272' : "#83fc93"}]} onPress={() => handleButton()}>
+      <TouchableOpacity  style={[styles.button, {backgroundColor: status ? '#ff7272' : "#83fc93"}]} onPress={() => changeStatus()}>
         <Text style={styles.buttonText}>{status ? "Pause" : "Start"}</Text>
       </TouchableOpacity>
     </View>
